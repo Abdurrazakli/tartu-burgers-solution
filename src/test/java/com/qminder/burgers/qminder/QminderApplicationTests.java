da99c195e6ec80822b66fbecb43d132b00fe7e7e
Adil Abdurrazakli <aabdurrazakli@std.beu.edu.az>
2021-11-24 01:38:20 +0200

    Initial project

---
+++ src/test/java/com/qminder/burgers/qminder/QminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qminder.burgers.qminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
