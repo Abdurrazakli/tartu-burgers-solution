e8e904ce173ec595b7100392f39c249a033a6ada
Adil Abdurrazakli <aabdurrazakli@std.beu.edu.az>
2021-11-26 02:07:01 +0200

    Add Web client config

---
+++ src/main/java/com/qminder/burgers/qminder/config/Beans.java
@@ -0,0 +1,32 @@
+package com.qminder.burgers.qminder.config;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class Beans {
+    public static final int TIMEOUT = 5000;
+
+    @Bean("webClient")
+    public WebClient webClientWithTimeout() {
+        final var tcpClient = HttpClient
+                .create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
+                .doOnConnected(connection -> {
+                    connection.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+                });
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(tcpClient))
+                .build();
+    }
+}
